41df99ea682cadd9143aaf5809d67a081d036271
Ana López <anamuyamiga@gmail.com>
2024-02-03 19:35:35 +0100

    primeras graficas porque no se que hacer

--- analisisdatos.py
+++ analisisdatos.py
@@ -1,5 +1,5 @@
 import numpy as np 
-import matplotlib as plt
+import matplotlib.pyplot as plt
 import pandas as pd
 
 # Cargar los dataset
@@ -19,3 +19,42 @@ partidos = partidos.drop(['33', '34'], axis=1)
 # Printo los resultados
 print(equipo.head())
 print(partidos.head())
+
+# Ahora que hemos limpiado los datos, vamos a unir los dos dataset
+data = pd.merge(partidos, equipo, left_on='id_equipo_local', right_on='id_equipo')
+
+# Mostramos los primeros registros
+print(data.head())
+
+# Ahora vamos a hacer gráficos de los datos
+# Vamos a hacer un gráfico de barras con los goles que ha marcado cada equipo
+data.groupby('nombre_equipo')['goles_local'].sum().plot(kind='bar')
+plt.xlabel('Equipo')
+plt.ylabel('Goles')
+plt.title('Goles marcados por equipo')
+plt.show()
+
+# Vamos a graficar los 5 equipos que más goles han marcado
+data.groupby('nombre_equipo')['goles_local'].sum().sort_values(ascending=False).head().plot(kind='bar')
+plt.xlabel('Equipo')
+plt.ylabel('Goles')
+plt.title('Goles marcados por equipo')
+plt.show()
+
+
+
+# Gráfico de dispersión con el promedio de goles en casa por equipo
+promedio_goles_local = data.groupby('nombre_equipo')['goles_local'].mean()
+promedio_goles_local.plot(marker='o', linestyle='-', color='green')
+plt.xlabel('Equipo')
+plt.ylabel('Promedio de goles en casa')
+plt.title('Promedio de goles por partido en casa por equipo')
+plt.show()
+
+# Vamos a graficar el numero de partidos jugados por equipo
+partidos_jugados = data['nombre_equipo'].value_counts()
+partidos_jugados.plot(kind = 'bar')
+plt.xlabel('Equipo')
+plt.ylabel('Partidos Jugados')
+plt.title('Partidos jugados por equipo')
+plt.show()
