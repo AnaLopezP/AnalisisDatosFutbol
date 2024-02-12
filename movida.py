f21b8fa93ecd8a4a5117fe9904be33dc88565345
Ana López <anamuyamiga@gmail.com>
2024-02-12 11:14:16 +0100

    sin miedo al exito

--- movida.py
+++ movida.py
@@ -3,14 +3,19 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 # cargo los datos
-d_uefa = pd.read_csv('uefa-movida-bien.csv', delim_whitespace=True)
-#d_uefa.columns = ["Posicion", "Club", "Pais", "Participaciones", "Titulos", "Partidos_Jug", "Partidos_Gan", "Partidos_Empat", "Partidos_perd", "Goles_favor", "Goles_contra", "Puntos", "Diferencia_goles"]
+d_uefa = pd.read_csv('datos_uefa.csv', delimiter=',')
+d_uefa.columns = ["Posicion", "Club", "Pais", "Participaciones", "Titulos", "Partidos_Jug", "Partidos_Gan", "Partidos_Empat", "Partidos_perd", "Goles_favor", "Goles_contra", "Puntos", "Diferencia_goles"]
 print(d_uefa.head())
 
-# quito las comillas y que me molestan tio
-for dato in d_uefa:
-    d_uefa[dato] = d_uefa[dato].str.replace('"', '')
-    
+#Para hallar la probabilidad de ganar un partido, dividimos el numero de partidos ganados entre los jugados y multiplicamos por 100
+prob_ganar = (d_uefa['Partidos_Gan']/d_uefa['Partidos_Jug'])*100
+#lo mismo para empatar y perder:
+prob_perder = (d_uefa['Partidos_perd']/d_uefa['Partidos_Jug'])*100
+prob_empat = (d_uefa['Partidos_Empat']/d_uefa['Partidos_Jug'])*100
 
-# guardo los datos en un archivo nuevo
-d_uefa.to_csv('uefa.csv', index=False, sep=',')
+# Añadimos el dato a una nuevas columnas
+d_uefa['Prob_ganar'] = prob_ganar
+d_uefa['Prob_Empat'] = prob_empat
+d_uefa['Prob_perder'] = prob_perder
+
+print(d_uefa.head())
