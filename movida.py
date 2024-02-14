d9bbf66cba7daaaf130c585a112a64cd02074e31
Ana López <anamuyamiga@gmail.com>
2024-02-14 11:01:01 +0100

    probabilidades enfrentadas

--- movida.py
+++ movida.py
@@ -19,3 +19,21 @@ d_uefa['Prob_Empat'] = prob_empat
 d_uefa['Prob_perder'] = prob_perder
 
 print(d_uefa.head())
+
+# Con esto he obtenido las probabilidades individuales de cada equipo, pero ahora tengo que las probabilidades enfrentadas. Es decir, las prob de un equipo con las de otro equipo en un partido. 
+# Para ello, voy a hacer una función que me devuelva las probabilidades enfrentadas de dos equipos.
+
+def prob_enfrentada(equipo1, equipo2):
+    # con esto cogemos el valor de la probabilidad del equipo que coincide con el nombre del club que le pasamos
+    prob_ganar1 = d_uefa[d_uefa['Club'] == equipo1]['Prob_ganar'].values[0] 
+    prob_ganar2 = d_uefa[d_uefa['Club'] == equipo2]['Prob_ganar'].values[0]
+    prob_empat1 = d_uefa[d_uefa['Club'] == equipo1]['Prob_Empat'].values[0]
+    prob_empat2 = d_uefa[d_uefa['Club'] == equipo2]['Prob_Empat'].values[0]
+    prob_perder1 = d_uefa[d_uefa['Club'] == equipo1]['Prob_perder'].values[0]
+    prob_perder2 = d_uefa[d_uefa['Club'] == equipo2]['Prob_perder'].values[0]
+    # la probabilidad de ganar de un equipo es: prob ganar* prob perder del otro equipo. misma lógica en los otros dos casos
+    prob_enfrentada_ganar = (prob_ganar1*prob_perder2)/100
+    prob_enfrentada_empat = (prob_empat1*prob_empat2)/100
+    prob_enfrentada_perder = (prob_perder1*prob_ganar2)/100
+    print(prob_enfrentada_ganar, prob_enfrentada_empat, prob_enfrentada_perder)
+    return prob_enfrentada_ganar, prob_enfrentada_empat, prob_enfrentada_perder
