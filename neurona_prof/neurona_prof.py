4ce4e19b330e62a20df82a71180d69748013d566
Ana López <anamuyamiga@gmail.com>
2024-05-13 17:07:23 +0200

    ejkfjkfh

--- neurona_prof/neurona_prof.py
+++ neurona_prof/neurona_prof.py
@@ -9,64 +9,6 @@ from matplotlib import pyplot as plt
 from sklearn.metrics import confusion_matrix
 import numpy as np
 
-# Vamos a añadir una columna clasidficando el estilo de juego de los equipos en ataque o defensa
-# Si la probabilidad de meter gol es alta (mas 0.5) --> ataque
-# Si la probabilidad de recibir gol es baja (menos 0.5) --> defensa
-# Cargamos los datos
-d_uefa_ruta = os.path.join(os.path.dirname(__file__), 'datos_uefa_mejorados_clasif.csv')
-d_uefa = pd.read_csv(d_uefa_ruta, delimiter=',')
-
-def clasificar_estilo(row):
-    if row['Probabilidad_marcar_gol'] > 0.5 and row['Probabilidad_recibir_gol'] > 0.5:
-        return 1 # Ataque
-    if row['Probabilidad_marcar_gol'] < 0.5 and row['Probabilidad_recibir_gol'] < 0.5:
-        return 0 # Defensa
-    else:
-        return 2 # Equilibrado
-
-d_uefa['estiloFutbol'] = d_uefa.apply(clasificar_estilo, axis=1)
-
-# Guardmos el csv con la nueva columna
-d_uefa.to_csv(d_uefa_ruta, index=False)
-
-# Quito las columnas que no son numericas
-d_uefa = d_uefa.drop(columns=['Club', 'Pais'])
-
-#Ponemos una semilla aleatoria
-t.manual_seed(0)
-
-# Separamos 70-30 en train y test
-features = d_uefa.drop(columns=['Prob_ganar' ,'Prob_empatar','Prob_perder'])
-#label = d_uefa['Prob_ganar' ,'Prob_empatar','Prob_perder']
-label = d_uefa["estiloFutbol"]
-
-X_train, X_test, y_train, y_test = train_test_split(features, label, test_size=0.3, random_state=0)
-print(X_train, X_test, y_train, y_test)
-
-# miramos los primero 25 de entrenamiento
-for i in range(0, 24):
-    print(X_train.iloc[i], y_train.iloc[i])
-    
-print("HASTA AQUÍ TODO BIEN 1")
-    
-# Preparamos los datos para torch
-# Hacemos un dataset y un loader para los datos de entrenamiento
-train_X = t.Tensor(X_train.values).float()
-train_Y = t.Tensor(y_train.values).long()
-train_ds = td.TensorDataset(train_X, train_Y)
-train_loader = td.DataLoader(train_ds, batch_size=20, shuffle=True, num_workers=1)
-
-# Hacemos lo mismo para los datos de prueba
-test_X = t.Tensor(X_test.values).float()
-test_Y = t.Tensor(y_test.values).long()
-test_ds = td.TensorDataset(test_X, test_Y)
-test_loader = td.DataLoader(test_ds, batch_size=20, shuffle=True, num_workers=1)
-print("HASTA AQUÍ TODO BIEN")
-
-# Definimos la red neuronal
-#numero de capas ocultas
-hl = 10
-
 # definimos la red
 class UefaNet(nn.Module):
     def __init__(self):
@@ -135,6 +77,66 @@ optimizador = t.optim.Adam(modelo.parameters(), lr=learning_rate)
 #optimizador.zero_grad()
 
 if __name__ == '__main__':
+    
+    # Vamos a añadir una columna clasidficando el estilo de juego de los equipos en ataque o defensa
+    # Si la probabilidad de meter gol es alta (mas 0.5) --> ataque
+    # Si la probabilidad de recibir gol es baja (menos 0.5) --> defensa
+    # Cargamos los datos
+    d_uefa_ruta = os.path.join(os.path.dirname(__file__), 'datos_uefa_mejorados_clasif.csv')
+    d_uefa = pd.read_csv(d_uefa_ruta, delimiter=',')
+
+    def clasificar_estilo(row):
+        if row['Probabilidad_marcar_gol'] > 0.5 and row['Probabilidad_recibir_gol'] > 0.5:
+            return 1 # Ataque
+        if row['Probabilidad_marcar_gol'] < 0.5 and row['Probabilidad_recibir_gol'] < 0.5:
+            return 0 # Defensa
+        else:
+            return 2 # Equilibrado
+
+    d_uefa['estiloFutbol'] = d_uefa.apply(clasificar_estilo, axis=1)
+
+    # Guardmos el csv con la nueva columna
+    d_uefa.to_csv(d_uefa_ruta, index=False)
+
+    # Quito las columnas que no son numericas
+    d_uefa = d_uefa.drop(columns=['Club', 'Pais'])
+
+    #Ponemos una semilla aleatoria
+    t.manual_seed(0)
+
+    # Separamos 70-30 en train y test
+    features = d_uefa.drop(columns=['Prob_ganar' ,'Prob_empatar','Prob_perder'])
+    #label = d_uefa['Prob_ganar' ,'Prob_empatar','Prob_perder']
+    label = d_uefa["estiloFutbol"]
+
+    X_train, X_test, y_train, y_test = train_test_split(features, label, test_size=0.3, random_state=0)
+    print(X_train, X_test, y_train, y_test)
+
+    # miramos los primero 25 de entrenamiento
+    for i in range(0, 24):
+        print(X_train.iloc[i], y_train.iloc[i])
+        
+    print("HASTA AQUÍ TODO BIEN 1")
+        
+    # Preparamos los datos para torch
+    # Hacemos un dataset y un loader para los datos de entrenamiento
+    train_X = t.Tensor(X_train.values).float()
+    train_Y = t.Tensor(y_train.values).long()
+    train_ds = td.TensorDataset(train_X, train_Y)
+    train_loader = td.DataLoader(train_ds, batch_size=20, shuffle=True, num_workers=1)
+
+    # Hacemos lo mismo para los datos de prueba
+    test_X = t.Tensor(X_test.values).float()
+    test_Y = t.Tensor(y_test.values).long()
+    test_ds = td.TensorDataset(test_X, test_Y)
+    test_loader = td.DataLoader(test_ds, batch_size=20, shuffle=True, num_workers=1)
+    print("HASTA AQUÍ TODO BIEN")
+
+    # Definimos la red neuronal
+    #numero de capas ocultas
+    hl = 10
+
+    
     epoch_nums = []
     training_loss = []
     validation_loss = []
